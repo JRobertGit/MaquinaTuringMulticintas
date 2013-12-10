22f5aba1340c326005807c0d967d0b5df24198af
mcunille <mauriciocunille@hotmail.com>
2013-12-09 20:04:24 -0600

    Console Program - Step by step Complete!

--- TuringMachineMTConsoleApplication/Program.cs
+++ TuringMachineMTConsoleApplication/Program.cs
@@ -357,7 +357,7 @@ namespace TuringMachineMTConsoleApplication
         static void StepByStep(FormalDescription machineDescription)
         {
             string inputString = string.Empty;
-            int elementsPerPage = 3;
+            int elementsPerPage = 4;
 
             ScreenClearContent();
             ScreenWriteContent(0, " *** Step by Step Run ***");
@@ -394,11 +394,20 @@ namespace TuringMachineMTConsoleApplication
             ScreenWriteContent(1, string.Format(" Running machine for string '{0}'...", inputString));
             ScreenWriteContent(3, " Note: The 'H' above each tape indicates the head position");
 
+            int[] Maximums = new int[machine.GetFormalDescription().GetNumberOfTapes()];
+            int[] Minimums = new int[machine.GetFormalDescription().GetNumberOfTapes()];
+
+            for (int i = 0; i < Minimums.Length; i++ )
+            {
+                Maximums[i] = 9;
+                Minimums[i] = 0;
+            }
+
             do
             {
                 ScreenWriteContent(5, FormatToLeft(string.Format("Current state: {0} ", machine.GetCurrentStateName())));
 
-                string[][] HeadAndTapes = FormatTapes(machine, 11);
+                string[][] HeadAndTapes = FormatTapes(machine, Minimums, Maximums, out Minimums, out Maximums);
                 string[] HeadsStr = HeadAndTapes[0];
                 string[] TapesStr = HeadAndTapes[1];
 
@@ -416,7 +425,7 @@ namespace TuringMachineMTConsoleApplication
 
             } while (machine.NextStep());
 
-            ScreenWriteContent(16,CenterStringWithChar(' ', machine.GetMachineStatus().ToString()));
+            ScreenWriteContent(19,CenterStringWithChar(' ', machine.GetMachineStatus().ToString()));
             ScreenWriteContent(22, " Note: For showing all the final tapes press 's'.");
 
             bool validOption = true;
@@ -449,10 +458,13 @@ namespace TuringMachineMTConsoleApplication
 
         static void ShowTapes(TuringMachine machine)
         {
-            string[][] HeadAndTapes = FormatTapes(machine, 11);
+            string[][] HeadAndTapes = FormatTapes(machine);
             string[] HeadsStr = HeadAndTapes[0];
             string[] TapesStr = HeadAndTapes[1];
+            int[] TapeOffSet = new int[TapesStr.Length];
+
             int elementsPerPage = 5;
+            int tapeSizeOnScreen = 10 * 4 + 1;
             int pages = TapesStr.Length / elementsPerPage;
             int currentPage = 0;
             if (TapesStr.Length % elementsPerPage != 0 && pages > 0) pages++;
@@ -469,26 +481,51 @@ namespace TuringMachineMTConsoleApplication
                 {
                     if (currentPage * elementsPerPage + i >= TapesStr.Length)
                         break;
-                    ScreenWriteContent(3 * i + 4, string.Format("          \t{0}", HeadsStr[currentPage * elementsPerPage + i]));
-                    ScreenWriteContent(3 * i + 5, string.Format(" Tape {0}:\t{1}", currentPage * elementsPerPage + i, TapesStr[currentPage * elementsPerPage + i]));
+
+                    int elementNumber = currentPage * elementsPerPage + i;
+                    int formatLength = tapeSizeOnScreen > TapesStr[elementNumber].Length ? TapesStr[elementNumber].Length : tapeSizeOnScreen;
+
+                    string formattedHead = HeadsStr[elementNumber].Substring(TapeOffSet[elementNumber], formatLength);
+                    string formattedTape = TapesStr[elementNumber].Substring(TapeOffSet[elementNumber], formatLength);
+
+                    string prevHead = "      ";
+                    string nextHead = string.Empty;
+                    string prevTape = "      ";
+                    string nextTape = string.Empty;
+
+                    if (TapeOffSet[elementNumber] > 0)
+                    {
+                        prevTape = "| ... ";
+                    }
+
+                    if (TapeOffSet[elementNumber] + tapeSizeOnScreen < TapesStr[elementNumber].Length)
+                    {
+                        nextHead = "      ";
+                        nextTape = " ... |";
+                    }
+
+                    ScreenWriteContent(3 * i + 4, string.Format("          \t{1}{0}{2}", formattedHead, prevHead, nextHead));
+                    ScreenWriteContent(3 * i + 5, string.Format(" Tape {0}:\t{1}{3}{2}", elementNumber, prevTape, nextTape, formattedTape));
                 }
+
+                ScreenWriteContent(22, " You can move a tape by pressing 'R' or 'L' followed by the tape number.");
 
                 if (pages > 0)
                 {
                     if (currentPage == 0)
                     {
-                        ScreenWriteContent(22, " Press 'n' for next page");
-                        ScreenWriteContent(21, string.Empty);
+                        ScreenWriteContent(19, string.Empty);
+                        ScreenWriteContent(20, " Press 'n' for next page");
                     }
                     else if (currentPage == pages - 1)
                     {
-                        ScreenWriteContent(22, " Press 'p' for prev page");
-                        ScreenWriteContent(21, string.Empty);
+                        ScreenWriteContent(19, string.Empty);
+                        ScreenWriteContent(20, " Press 'p' for prev page");
                     }
                     else
                     {
-                        ScreenWriteContent(21, " Press 'n' for next page");
-                        ScreenWriteContent(22, " Press 'p' for prev page");
+                        ScreenWriteContent(19, " Press 'p' for prev page");
+                        ScreenWriteContent(20, " Press 'n' for next page");
                     }
                 }
 
@@ -500,9 +537,9 @@ namespace TuringMachineMTConsoleApplication
 
                 if (option.Equals("r")) return;
 
-                switch (option)
+                switch (option[0])
                 {
-                    case "n":
+                    case 'n':
                         if (pages > 0 && currentPage < pages - 1)
                         {
                             validOption = true;
@@ -513,7 +550,7 @@ namespace TuringMachineMTConsoleApplication
                             validOption = false;
                         }
                         break;
-                    case "p":
+                    case 'p':
                         if (pages > 0 && currentPage > 0)
                         {
                             validOption = true;
@@ -524,6 +561,29 @@ namespace TuringMachineMTConsoleApplication
                             validOption = false;
                         }
                         break;
+                    case 'R':
+                        int tapeID = -1;
+                        validOption = Int32.TryParse(option[1].ToString(), out tapeID);
+                        if (tapeID >= 0 && tapeID < TapesStr.Length)
+                        {
+                            if (TapeOffSet[tapeID] + tapeSizeOnScreen + 4 <= TapesStr[tapeID].Length)
+                                TapeOffSet[tapeID] += 4;
+                            else validOption = false;
+                        }
+                        else validOption = false;
+                        break;
+                    case 'L':
+                        tapeID = -1;
+                        validOption = Int32.TryParse(option[1].ToString(), out tapeID);
+                        if (tapeID >= 0 && tapeID < TapesStr.Length)
+                        {
+                            if (TapeOffSet[tapeID] - 4 >= 0)
+                                TapeOffSet[tapeID] -= 4;
+                            else
+                                validOption = false;
+                        }
+                        else validOption = false;
+                        break;
                     default:
                         validOption = false;
                         break;
@@ -532,7 +592,7 @@ namespace TuringMachineMTConsoleApplication
 
         }
 
-        static string[][] FormatTapes(TuringMachine machine, int SmallestSize)
+        static string[][] FormatTapes(TuringMachine machine)
         {
             int numberOfTapes = machine.GetFormalDescription().GetNumberOfTapes();
             string[] TapesStr = new string[numberOfTapes];
@@ -544,14 +604,67 @@ namespace TuringMachineMTConsoleApplication
                 char[] tape = machine.GetTapeContent(i);
                 int head = machine.GetHeadLocation(i);
 
-                string unformattedTape;
-                if (head > SmallestSize - 1)
+                string unformattedTape = new string(tape);
+                string formattedTape = string.Empty;
+                string formattedHead = string.Empty;
+                for (int j = 0; j < unformattedTape.Length; j++)
                 {
-                    unformattedTape = new string(tape, head - SmallestSize, SmallestSize);
-                    head = SmallestSize - 1;
+                    if (j == 0)
+                        formattedTape = string.Format("| {0} |", unformattedTape[j]);
+                    else
+                        formattedTape = string.Format("{0} {1} |", formattedTape, unformattedTape[j]);
+
+                    if (j == head && j == 0)
+                        formattedHead = string.Format("  H  ");
+                    else if (j == head)
+                        formattedHead = string.Format("{0} H  ", formattedHead);
+                    else if (j != head && j == 0)
+                        formattedHead = string.Format("     ");
+                    else
+                        formattedHead = string.Format("{0}    ", formattedHead);
                 }
-                else
-                    unformattedTape = new string(tape, 0, SmallestSize);
+                
+                TapesStr[i] = formattedTape;
+                HeadStr[i] = formattedHead;
+            }
+
+            HeadAndTape[0] = HeadStr;
+            HeadAndTape[1] = TapesStr;
+
+            return HeadAndTape;
+        }
+
+        static string[][] FormatTapes(TuringMachine machine, int[] Minimums, int[] Maximums, out int[] MinimumsO, out int[] MaximumsO)
+        {
+            int numberOfTapes = machine.GetFormalDescription().GetNumberOfTapes();
+            string[] TapesStr = new string[numberOfTapes];
+            string[] HeadStr = new string[numberOfTapes];
+            string[][] HeadAndTape = new string[4][];
+
+            for (int i = 0; i < numberOfTapes; i++)
+            {
+                char[] tape = machine.GetTapeContent(i);
+                int head = machine.GetHeadLocation(i);
+
+                if (Maximums[i] >= tape.Length)
+                    Maximums[i] = tape.Length - 1;
+
+                if (head > Maximums[i])
+                {
+                    Minimums[i] += head - Maximums[i];
+                    Maximums[i] = head;
+                    head = Maximums[i] - Minimums[i];
+                } else if (head < Minimums[i])
+                {
+                    Maximums[i] -= (Minimums[i] - head);
+                    Minimums[i] = head;
+                    head = 0;
+                } else
+                {
+                    head = head - Minimums[i];
+                }
+
+                string unformattedTape = new string(tape, Minimums[i], Maximums[i] - Minimums[i] + 1);
 
                 string formattedTape = string.Empty;
                 string formattedHead = string.Empty;
@@ -571,13 +684,15 @@ namespace TuringMachineMTConsoleApplication
                     else
                         formattedHead = string.Format("{0}    ", formattedHead);
                 }
-                
-                TapesStr[i] = string.Format("{0} ... |", formattedTape);
+
+                TapesStr[i] = string.Format("{0} ... |",formattedTape);
                 HeadStr[i] = formattedHead;
             }
 
             HeadAndTape[0] = HeadStr;
             HeadAndTape[1] = TapesStr;
+            MinimumsO = Minimums;
+            MaximumsO = Maximums;
 
             return HeadAndTape;
         }
